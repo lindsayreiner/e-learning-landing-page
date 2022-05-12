41ce988520555b95e7e16ac8b935124babdc2ce6
Lindsay Reiner <lindsayreiner@hey.com>
2022-05-11 23:06:50 -0400

    removed separate components, added and styled navbar and hero image for desktop

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import '../styles/LandingPage.css'
+
+import heroImg from '../assets/image-hero-desktop.png'
 
 export default function LandingPage() {
     return (
-        <div>LandingPage</div>
+        <>
+            <nav className="navBar">
+                <h1 className="company">skilled</h1>
+                <button className="getStartedBtn">Get Started</button>
+            </nav>
+
+            <section className="heroCont">
+                <div className="heroTextCont">
+                    <h2 className="heroTitle">Maximize skill, minimize budget</h2>
+                    <p className="heroText">Our modern courses across a range of in-demand skills will give you the
+                        knowledge you need to live the life you want.</p>
+                </div>
+
+                <img className="heroImg" src={heroImg} alt="woman-drinking-coffee" height="800" />
+            </section>
+        </>
     )
 }

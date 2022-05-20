55cfd387d407a9c74a4e1a6671aff9a252376e0f
Lindsay Reiner <lindsayreiner@hey.com>
2022-05-19 22:21:59 -0400

    added footer, additional formatting for courses

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -8,7 +8,7 @@ export default function LandingPage() {
         <>
             <nav className="navBar">
                 <h1 className="company">skilled</h1>
-                <button className="getStartedBtn">Get Started</button>
+                <button className="getStartedBtn blue">Get Started</button>
             </nav>
 
             <section className="heroCont">
@@ -20,36 +20,41 @@ export default function LandingPage() {
 
                 <img className="heroImg" src={heroImg} alt="woman-drinking-coffee" height="800" />
             </section>
-            <section className="coursesCont">
-                <div>Check out our most popular courses!</div>
-                <div className="courseCard">
-                    <span className="courseTitle">Animation</span>
-                    <p> Learn the latest animation techniques to create stunning motion design and captivate your audience.</p>
-                    <a href="url">Get Started</a>
+            <section className="coursesGrid">
+                <h3 className="checkOut grid-padding">Check out our most popular courses!</h3>
+                <div className="course grid-padding animation">
+                    <h4 className="courseTitle">Animation</h4>
+                    <p className="courseDesc"> Learn the latest animation techniques to create stunning motion design and captivate your audience.</p>
+                    <a href="url" className="course-get-started">Get Started</a>
                 </div>
 
-                <div className="courseCard">
-                    <span className="courseTitle">Design</span>
+                <div className="course grid-padding design">
+                    <h4 className="courseTitle">Design</h4>
                     <p className="courseDesc"> Create beautiful, usable interfaces to help shape the future of how the web looks.</p>
-                    <a href="url">Get Started</a>
+                    <a href="url" className="course-get-started">Get Started</a>
 
                 </div>
-                <div className="courseCard">
-                    <span className="courseTitle">Photography</span>
+                <div className="course grid-padding photography">
+                    <h4 className="courseTitle">Photography</h4>
                     <p className="courseDesc"> Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.</p>
-                    <a href="url">Get Started</a>
+                    <a href="url" className="course-get-started">Get Started</a>
                 </div>
-                <div className="courseCard">
-                    <span className="courseTitle">Crypto</span>
+                <div className="course grid-padding crypto">
+                    <h4 className="courseTitle">Crypto</h4>
                     <p className="courseDesc">All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.</p>
-                    <a href="url">Get Started</a>
+                    <a href="url" className="course-get-started">Get Started</a>
                 </div>
-                <div className="courseCard">
-                    <span className="courseTitle">Business</span>
+                <div className="course grid-padding business">
+                    <h4 className="courseTitle">Business</h4>
                     <p className="courseDesc"> A step-by-step playbook to help you start, scale, and sustain your business without outside investment.</p>
-                    <a href="url">Get Started</a>
+                    <a href="url" className="course-get-started">Get Started</a>
                 </div>
             </section>
+
+            <footer className="footer">
+                <h5 className="company">skilled</h5>
+                <button className="getStartedBtn pink">Get Started</button>
+            </footer>
         </>
     )
 }
